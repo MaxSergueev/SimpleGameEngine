ace22161c95b5c816c538b803f3f9028c9331471
Max Sergueev <max.sergueev@artfx.fr>
2025-03-14 16:09:22 +0100

    [Style] Changed GLTestScene to initialise transform with variables

--- SimpleGameEngine/GlTestScene.cpp
+++ SimpleGameEngine/GlTestScene.cpp
@@ -3,7 +3,9 @@
 #include "SpriteComponent.h"
 
 Actor* actor;
-Vector3 rotation{ 0, 0, 0 };
+Vector3 initialPosition{ 0, 0, 1000 };
+Vector3 initialRotation{ 0, 0, 0};
+Vector3 initialScale{ 0.2, 0.2, 0.2 };
 
 GlTestScene::GlTestScene()
 {
@@ -21,6 +23,7 @@ void GlTestScene::Render()
 void GlTestScene::Update()
 {
 	actor->Rotate(Vector3(1, 0, 1));
+	//actor->RotateAroundAxis(rotation, 2);
 }
 
 void GlTestScene::Close()
@@ -38,9 +41,9 @@ void GlTestScene::Load()
 
 	actor = new Actor();
 	AddActor(actor);
-	actor->SetPosition(Vector3{ 0, 0, 500 });
-	actor->SetRotation(Vector3{ rotation });
-	actor->SetScale(Vector3{ 0.2, 0.2, 0.2 });
+	actor->SetPosition(initialPosition);
+	actor->SetRotation(initialRotation);
+	actor->SetScale(initialScale);
 
 	Assets::LoadTexture(*mRenderer, "Resources/pokeball.png", "ball");
 	SpriteComponent* sprite = new SpriteComponent(actor, Assets::GetTexture("ball"));
